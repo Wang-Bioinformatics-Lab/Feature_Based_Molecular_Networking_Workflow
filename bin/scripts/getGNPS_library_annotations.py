2a2103bfa8955b4b5a605eab4082da48d55a88da
Mingxun Wang <mwang87@gmail.com>
2023-08-16 17:45:29 -0700

    updating library results

--- bin/scripts/getGNPS_library_annotations.py
+++ bin/scripts/getGNPS_library_annotations.py
@@ -14,6 +14,11 @@ def enrich_output(input_filename, output_filename, topk=None):
     spectrum_id_cache = {}
     molecule_explorer_df = pd.DataFrame(ming_gnps_library.get_molecule_explorer_dataset_data())
 
+    if not os.path.exists(input_filename):
+        open(output_filename, "w").close()
+        print("Input file does not exist")
+        exit(0)
+    
     input_results_df = pd.read_csv(input_filename, sep="\t")
 
     # Here we will try to filter to topk
@@ -142,7 +147,7 @@ def enrich_output(input_filename, output_filename, topk=None):
         # Calculating inchi
         if len(output_result_dict["Smiles"]) > 5 and len(output_result_dict["INCHI"]) < 5:
             try:
-                inchi_url = "https://gnps-structure.ucsd.edu/inchi?smiles={}".format(urllib.parse.quote_plus(output_result_dict["Smiles"]), 
+                inchi_url = "https://structure.gnps2.org/inchi?smiles={}".format(urllib.parse.quote_plus(output_result_dict["Smiles"]), 
                                     urllib.parse.quote_plus(output_result_dict["INCHI"]))
                 r = requests.get(inchi_url)
                 r.raise_for_status()
@@ -153,7 +158,7 @@ def enrich_output(input_filename, output_filename, topk=None):
         # Calculating smiles
         if len(output_result_dict["Smiles"]) < 5 and len(output_result_dict["INCHI"]) > 5:
             try:
-                smiles_url = "https://gnps-structure.ucsd.edu/smiles?inchi={}".format(urllib.parse.quote_plus(output_result_dict["INCHI"]), 
+                smiles_url = "https://structure.gnps2.org/smiles?inchi={}".format(urllib.parse.quote_plus(output_result_dict["INCHI"]), 
                                     urllib.parse.quote_plus(output_result_dict["Smiles"]))
                 r = requests.get(smiles_url)
                 r.raise_for_status()
@@ -164,7 +169,7 @@ def enrich_output(input_filename, output_filename, topk=None):
         # Calculating molecular formula
         if len(output_result_dict["Smiles"]) > 5:
             try:
-                formula_url = "https://gnps-structure.ucsd.edu/formula?smiles={}".format(output_result_dict["Smiles"])
+                formula_url = "https://structure.gnps2.org/formula?smiles={}".format(output_result_dict["Smiles"])
                 r = requests.get(formula_url)
                 r.raise_for_status()
                 output_result_dict["molecular_formula"] = r.text
@@ -179,7 +184,7 @@ def enrich_output(input_filename, output_filename, topk=None):
             output_result_dict["InChIKey-Planar"] = "N/A"
         else:
             try:
-                inchikey_url = "https://gnps-structure.ucsd.edu/inchikey?smiles={}&inchi={}".format(urllib.parse.quote_plus(output_result_dict["Smiles"]), 
+                inchikey_url = "https://structure.gnps2.org/inchikey?smiles={}&inchi={}".format(urllib.parse.quote_plus(output_result_dict["Smiles"]), 
                                     urllib.parse.quote_plus(output_result_dict["INCHI"]))
                 r = requests.get(inchikey_url)
                 r.raise_for_status()
@@ -192,7 +197,7 @@ def enrich_output(input_filename, output_filename, topk=None):
         # Getting Classyfire
         if len(output_result_dict["InChIKey"]) > 5:
             try:
-                classyfire_url = "https://gnps-classyfire.ucsd.edu/entities/{}.json".format(output_result_dict["InChIKey"])
+                classyfire_url = "https://classyfire.gnps2.org/entities/{}.json".format(output_result_dict["InChIKey"])
                 r = requests.get(classyfire_url, timeout=10)
                 r.raise_for_status()
                 classification_json = r.json()
@@ -212,7 +217,7 @@ def enrich_output(input_filename, output_filename, topk=None):
         # Getting NP Classifier
         if len(output_result_dict["Smiles"]) > 5:
             try:
-                npclassifier_url = "https://npclassifier.ucsd.edu/classify?smiles={}".format(output_result_dict["Smiles"])
+                npclassifier_url = "https://npclassifier.gnps2.org/classify?smiles={}".format(output_result_dict["Smiles"])
                 r = requests.get(npclassifier_url, timeout=10)
                 r.raise_for_status()
                 classification_json = r.json()
