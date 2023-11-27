393357873ebbc86aba6dfc588eef8b1ae82c0949
Mingxun Wang <mwang87@gmail.com>
2023-11-27 09:26:04 -0800

    bump version

--- bin/scripts/create_network_graphml.py
+++ bin/scripts/create_network_graphml.py
@@ -29,12 +29,14 @@ def convert_network(G):
                 new_key = column.upper()
             else:
                 new_key = "ATTRIBUTE_GNPS:{}".format(column.replace("GNPSGROUP:", ""))
-
-            # Trying to write it, but if its missing, lets skip it
+            
             try:
                 new_G.nodes[node][new_key] = float(G.nodes[node][column])
             except:
-                pass
+                try:
+                    new_G.nodes[node][new_key] = G.nodes[node][column]
+                except:
+                    pass
 
         # Fixing node attributes
         new_G.nodes[node]["mz"] = float("{:.4f}".format(float(G.nodes[node]["parent mass"])))
@@ -44,7 +46,6 @@ def convert_network(G):
         new_G.nodes[node]["component"] = G.nodes[node]["component"]
 
         node_to_component[node] = G.nodes[node]["component"]
-        
         
         if "Compound_Name" in G.nodes[node]:
             new_G.nodes[node]["library_compound_name"] = G.nodes[node]["Compound_Name"]
@@ -75,8 +76,11 @@ def main():
     parser.add_argument('input_pairs', help='input_pairs')
     parser.add_argument('input_library_matches', help='input_library_matches')
     parser.add_argument('output_graphml', help='output_graphml')
+    parser.add_argument('output_with_singleton_graphml', help='output_with_singleton_graphml')
 
     args = parser.parse_args()
+
+    # Parsing the normal network
 
     #Doing other filtering
     G = molecular_network_filtering_library.loading_network(args.input_pairs, hasHeaders=True)
@@ -89,6 +93,22 @@ def main():
     G = convert_network(G)
 
     nx.write_graphml(G, args.output_graphml, infer_numeric_types=True)
+
+    # Parsing the singleton network
+    G = molecular_network_filtering_library.loading_network(args.input_pairs, hasHeaders=True)
+    
+    # Adding the singletons into the network
+    molecular_network_filtering_library.add_singletons_to_network(G, args.input_clusterinfo_summary)
+
+    molecular_network_filtering_library.add_clusterinfo_summary_to_graph(G, args.input_clusterinfo_summary)
+    molecular_network_filtering_library.add_library_search_results_to_graph(G, args.input_library_matches)
+
+    # Cleaning up network when the clusterinfo summary is not present 
+
+    # Reformatting
+    G = convert_network(G)
+
+    nx.write_graphml(G, args.output_with_singleton_graphml, infer_numeric_types=True)
 
 
 
